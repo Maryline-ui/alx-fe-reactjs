e0017cf1ec3c70e9aea6ac6551f813cb5fb6a965
Maryline Abongo <marylineabongo@gmail.com>
2025-07-19 20:46:18 +0300

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '10px', position: 'fixed', left: 0, bottom: 0, width: '100%' }}>
+      <p>&copy; 2025 ALX React Project</p>
+    </footer>
+  );
+}
+
+export default Footer;
