1ac2b323b32f41cc9e616a74d64f0c7b620bdb13
Maryline Abongo <marylineabongo@gmail.com>
2025-07-21 12:30:02 +0300

    react app

--- alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '10px', position: 'fixed', left: 0, bottom: 0, width: '100%' }}>
-      <p>&copy; 2025 ALX React Project</p>
-    </footer>
-  );
-}
-
-export default Footer;
