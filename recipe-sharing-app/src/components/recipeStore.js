bcc9047825f10cab2229164753751419d38d251d
Maryline-ui <marylineabongo@gmail.com>
2025-07-27 02:32:33 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,15 +2,18 @@ import { create } from 'zustand';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (recipe) => set((state) => ({
-    recipes: [...state.recipes, recipe]
-  })),
-  deleteRecipe: (id) => set((state) => ({
-    recipes: state.recipes.filter((r) => r.id !== id)
-  })),
-  editRecipe: (updatedRecipe) => set((state) => ({
-    recipes: state.recipes.map((r) =>
-      r.id === updatedRecipe.id ? updatedRecipe : r
-    ),
-  })),
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id),
+    })),
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((r) =>
+        r.id === updatedRecipe.id ? updatedRecipe : r
+      ),
+    })),
 }));
