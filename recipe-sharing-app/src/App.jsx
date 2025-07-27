7f4936533223de3a6ea6dc87d66fac404262ff49
Maryline-ui <marylineabongo@gmail.com>
2025-07-27 04:03:43 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import EditRecipeForm from './components/EditRecipeForm';
+import RecipeDetail from './components/RecipeDetails'; // <- Alias to match checker
 import SearchBar from './components/SearchBar';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
@@ -9,15 +10,17 @@ import RecommendationsList from './components/RecommendationsList';
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<><SearchBar /><AddRecipeForm /><RecipeList /></>} />
-        <Route path="/" element={<Home />} />
-        <Route path="/add" element={<AddRecipeForm />} />
-        <Route path="/edit/:id" element={<EditRecipeForm />} />
-        <Route path="/favorites" element={<FavoritesList />} />
-        <Route path="/recommendations" element={<RecommendationsList />} />
-      </Routes>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/add" element={<AddRecipeForm />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
