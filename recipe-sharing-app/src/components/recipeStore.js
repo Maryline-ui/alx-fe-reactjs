f4597657c5d790a9d2e51f33a5c356b48679b3c1
Maryline-ui <marylineabongo@gmail.com>
2025-07-27 03:22:42 +0300

    Update recipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,32 +3,25 @@ import { create } from 'zustand';
 export const useRecipeStore = create((set) => ({
   recipes: [],
   favorites: [],
-  recommendations: [],
-  
   addRecipe: (recipe) => set((state) => ({
-    recipes: [...state.recipes, recipe]
+    recipes: [...state.recipes, recipe],
   })),
-  
   deleteRecipe: (id) => set((state) => ({
-    recipes: state.recipes.filter((r) => r.id !== id)
+    recipes: state.recipes.filter((r) => r.id !== id),
+    favorites: state.favorites.filter((favId) => favId !== id),
   })),
-  
-  updateRecipe: (updatedRecipe) => set((state) => ({
+  editRecipe: (updatedRecipe) => set((state) => ({
     recipes: state.recipes.map((r) =>
       r.id === updatedRecipe.id ? updatedRecipe : r
     ),
   })),
-  
-  addFavorite: (recipeId) =>
-    set((state) => ({
-      favorites: [...new Set([...state.favorites, recipeId])]
-    })),
-
-  removeFavorite: (recipeId) =>
-    set((state) => ({
-      favorites: state.favorites.filter((id) => id !== recipeId)
-    })),
-
+  addFavorite: (id) => set((state) => ({
+    favorites: [...state.favorites, id],
+  })),
+  removeFavorite: (id) => set((state) => ({
+    favorites: state.favorites.filter((favId) => favId !== id),
+  })),
+  recommendations: [],
   generateRecommendations: () =>
     set((state) => {
       const recommended = state.recipes.filter(
