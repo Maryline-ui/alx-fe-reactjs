f198d71782a4c7dc5a0c0b4254debf9aa9255594
Maryline Abongo <marylineabongo@gmail.com>
2025-08-23 21:41:54 +0300

    fixing FormRegistration.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
 import FormikForm from './components/FormikForm';
 import { DndProvider } from 'react-dnd';
